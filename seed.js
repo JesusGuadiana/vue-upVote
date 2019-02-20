f290bc1b4713469d0fdcb0dc931293d709be2f86
Jesus Guadiana <jisus130@hotmail.com>
2019-02-19 20:32:12 -0600

    Done

---
+++ seed.js
@@ -0,0 +1,42 @@
+window.Seed = (function () {
+const submissions = [
+  {
+    id: 1,
+    title: "League of legends S9 starts!!",
+    description: "Today the season starts get ready!!",
+    url: '#',
+    votes: 16,
+    avatar: 'public/images/avatars/daniel.jpg',
+    submissionImage: 'public/images/submissions/lol.png'
+  },
+  {
+    id: 2,
+    title: "OP CHAMPS OF THE PATCH",
+    description: "The following champs are op atm",
+    url: '#',
+    votes: 25,
+    avatar: 'public/images/avatars/kristy.png',
+    submissionImage: 'public/images/submissions/zed.png'
+  },
+  {
+    id: 3,
+    title: "Insane Lee Play!!",
+    description: "Look at my soloQ Lee Play!!",
+    url: '#',
+    votes: 5,
+    avatar: 'public/images/avatars/molly.png',
+    submissionImage: 'public/images/submissions/lee.png'
+  },
+  {
+    id: 4,
+    title: "Anyone on NA?",
+    description: "Looking for NA players",
+    url: '#',
+    votes: 1,
+    avatar: 'public/images/avatars/daniel.jpg',
+    submissionImage: 'public/images/submissions/question.png'
+  },
+]
+
+ return { submissions: submissions };
+ }());
